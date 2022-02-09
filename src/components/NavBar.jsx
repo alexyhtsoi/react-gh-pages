048ebf892c636bc54803b2344b90a42039b50a2a
AX Concepts <93191217+alexyhtsoi@users.noreply.github.com>
2022-02-09 21:21:34 +0800

    remove eslint

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,9 @@ const NavBar = () => {
     return <div> 
         <h2>This is the Navigation Menu</h2>
         <p>Steps to use GH Pages
-            1. Install gh-pages: npm install gh-pages --save
-            2. Add homepage to package.json:  "homepage": "https://milind452.github.io/my-reads/"
-            3. Add scripts: "predeploy": "npm run build",  "deploy": "gh-pages -d build"
+            1. Install gh-pages: npm install gh-pages --save <br/>
+            2. Add homepage to package.json:  "homepage": "https://milind452.github.io/my-reads/" <br/>
+            3. Add scripts: "predeploy": "npm run build",  "deploy": "gh-pages -d build" <br/>
             4. Deploy: npm run deploy
         </p>
     </div>;
