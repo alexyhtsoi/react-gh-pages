df8b65bc95e136e633ab9141d1f6140bd0fde98f
AX Concepts <93191217+alexyhtsoi@users.noreply.github.com>
2022-02-09 17:09:34 +0800

    Revert "Update to NavBar"
    This reverts commit 98867a89e34197f696d1fe9dfd6787afca492e62.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,6 @@
 const NavBar = () => {
     return <div> 
         <h2>This is the Navigation Menu</h2>
-        <p>Steps to use GH Pages
-            1. Install gh-pages: npm install gh-pages --save
-            2. Add homepage to package.json:  "homepage": "https://milind452.github.io/my-reads/"
-            3. Add scripts: "predeploy": "npm run build",  "deploy": "gh-pages -d build"
-            4. Deploy: npm run deploy
-        </p>
     </div>;
 }
 
